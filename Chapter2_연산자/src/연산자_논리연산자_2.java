5650aa2a05af270db92755c276d2e6d7bfa900b3
user <user@DESKTOP-9E0LFRE>
2024-04-15 17:52:22 +0900

    1111

---
+++ Chapter2_연산자/src/연산자_논리연산자_2.java
@@ -0,0 +1,31 @@
+// 점수 >=60 <=100  => ㅅ겯
+public class 연산자_논리연산자_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int score=(int)(Math.random()*100)+1; // 1~100 난수 발생
+        System.out.println("난수값:"+score);
+        
+        boolean bCheck=(score>=60) && (score%2==0);
+        //                             짝수 조건
+        System.out.println(bCheck);
+        // score가 40이상이고 score 홀수면 score%2==1 score%2!=0
+        // 재사용 => 변수만 사용
+        bCheck=(score>=40) && (score%2!=0);
+        System.out.println(bCheck);
+        
+        int a=10;
+        a++;
+        a=100;
+        a++;
+        a=200;
+        a++;
+        a=300;
+        a--;
+        System.out.println(a);
+        // 변수 => 필요시마다 변경해서 사용이 가능
+        // mail이 종료시까지 메모리가 유지
+        
+	}
+
+}
---
+++ Chapter2_연산자/src/연산자_단항연산자2.java
@@ -0,0 +1,14 @@
+// 부정연산자
+// ! (NOT) ==> boolean 변수에서만 사용이 가능  -> 부정이고 반대
+// 턴 
+public class 연산자_단항연산자2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //boolean bCheck=false;
+        //System.out.println(bCheck);
+        // System.out.println(!bCheck);
+		
+	}
+
+}
