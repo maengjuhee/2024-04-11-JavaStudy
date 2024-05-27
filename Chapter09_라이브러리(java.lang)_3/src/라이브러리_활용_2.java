8cfc5dc75804af3b3ca0791c9cce667bae135992
user <user@DESKTOP-9E0LFRE>
2024-05-28 08:31:06 +0900

    240522

---
+++ Chapter09_라이브러리(java.lang)_3/src/라이브러리_활용_2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+// 웹 => 오라클 (숫자, 문자열, 날짜)
+// 변수=데이터
+// --- 데이터와 같거나 크다 => 작은 경우에는 형변환
+public class 라이브러리_활용_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //ArrayList<Sawon> list=new ArrayList<Sawon>();
+        // 배열의 단점을 보완 => 가변성
+        
+        int a=(int)10.5;
+       // A     (A)Object
+        double d=10;
+        // Object = A
+        // 클래스는 관련된 것 묶어서 저장 (Object) A
+        
+        
+        
+        
+        
+//        list.add("홍길동"); //0
+//        list.add("심청이"); //1
+//        list.add(100); //2
+//        list.add(10.5); //3
+//        list.add(true); //4
+//        
+//        String name=(String)list.get(0); // Object가 커서 String에 못 들어가니 형변환
+//        double d=(double)list.get(3); // 언박싱 => 기본형에 클래스 대입하는 것
+        
+        
+	}
+
+}
